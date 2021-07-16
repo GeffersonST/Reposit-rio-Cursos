42bde252aa0e0cee5a07b29f30ebb8552b969ae2
GeffersonST <geffsevero@gmail.com>
2021-07-16 00:00:28 -0300

    Exer Curso JS

---
+++ Curso Javascript/Aula 14 ex/Modelo/script.js
@@ -0,0 +1,12 @@
+/* var ini = document.getElementById('txtin')
+var fim = document.getElementById('txtfim')
+var pass = document.getElementById('txtp')*/
+
+for (var ini = ini ; ini == fim; c++) { 
+  var ini = document.getElementById('txtin')
+  var fim = document.getElementById('txtfim')
+  var pass = document.getElementById('txtp')
+  
+  console.log(ini+fim)
+}
+console.log('Contagem é ${ini+fim}')

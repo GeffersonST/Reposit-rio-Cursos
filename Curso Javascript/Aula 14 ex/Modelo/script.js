044e527bde7e57098e1485fa0c9499386b24ba61
GeffersonST <geffsevero@gmail.com>
2021-07-16 13:40:39 -0300

    Exercícios JS curso

--- Curso Javascript/Aula 14 ex/Modelo/script.js
+++ Curso Javascript/Aula 14 ex/Modelo/script.js
@@ -1,12 +1,39 @@
-/* var ini = document.getElementById('txtin')
-var fim = document.getElementById('txtfim')
-var pass = document.getElementById('txtp')*/
+function contar() {
 
-for (var ini = ini ; ini == fim; c++) { 
-  var ini = document.getElementById('txtin')
-  var fim = document.getElementById('txtfim')
-  var pass = document.getElementById('txtp')
-  
-  console.log(ini+fim)
+
+
+
+  var ini = document.getElementById('txti')
+  var fim = document.getElementById('txtf')
+  var passo = document.getElementById('txtp')
+  var res = document.getElementById('res')
+
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    window.alert('[ERRO] Faltam dados!')}
+  else {
+
+        res.innerHTML = 'Contando: <br> '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <=0) { window.alert('Passo inválido! Considerando PASSO 1')
+      p = 1}
+        if (i < f) {
+          for (var c=i; c <=f; c+= p){
+            res.innerHTML += `${c} \u{1F449}`
+          }
+          
+
+        }
+        else {
+            for (var c=i; c>=f; c -= p) {
+              res.innerHTML += ` ${c} \u{1F449}`
+            }
+           
+        }
+          res.innerHTML += `\u{1F3C1}`
+        
+
+  }
+
 }
-console.log('Contagem é ${ini+fim}')
